9681df24d579403d168f42f9134a9adaf3db5103
wangfy263 <wfy339095626@gmail.com>
2019-07-14 11:29:27 +0800

    导出界面增加输入周期

--- src/page/weekly/export.vue
+++ src/page/weekly/export.vue
@@ -1,6 +1,6 @@
 <template>
   <div style="font-size: 14px">
-    <Input v-model="fileName" placeholder="可选，自定义导出文件名称" style="width: 300px" />@{{weekRange}}.xlsx
+    <Input v-model="fileName" placeholder="可选，自定义导出文件名称" style="width: 300px" />@<Input v-model="weekRange" placeholder="请输入日报周期，示例：0701-0705" style="width:150px" :maxlength="10"/>.xlsx
     <Button type="success" @click="exportExcel()">导出</Button>
   </div>
 </template>
@@ -17,7 +17,7 @@ export default {
   methods: {
     exportExcel () {
       this.$Spin.show()
-      exportExcel({ fileName: this.fileName }).then(res => {
+      exportExcel({ fileName: this.fileName, week_range: this.weekRange }).then(res => {
         if (res.data.retCode === '000000') {
           this.$Spin.hide()
           this.$Message.success('导出成功!')
